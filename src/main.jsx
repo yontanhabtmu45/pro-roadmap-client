fd1d18258eabfef96f75ee44e49638eb9ae3e641
Jhan-hab <201286123+yontanhabtmu45@users.noreply.github.com>
2025-12-11 00:28:27 +0300

    fixed

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,8 @@ import App from "./App.jsx";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { AuthProvider } from "./Contexts/AuthContext";
 
-
+const clientId =
+  "1083223003406-b78mdntufb75hg2srm1uuritag9bcf8f.apps.googleusercontent.com";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
